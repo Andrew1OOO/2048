64324b5accea08f6e2064fde84c708b02e034b10
Andrew <andrewsamdiab@gmail.com>
2021-07-13 16:19:35 -0400

    added a tkinter window

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
+import tkinter as tk
+from tkinter import *
+import tkinter
+import os
+
+from pygame.constants import QUIT
 from board import Board
 from block import Block
 import copy
 import random
 import pygame as pg
 
-game_exit = False
-BLACK = (0 ,0, 0)
+
 def round_rect(surf, rect, rad, color, thick=0):
 	if not rad:
 		pg.draw.rect(surf, color, rect, thick)
@@ -32,9 +37,9 @@ def round_rect(surf, rect, rad, color, thick=0):
 		pg.draw.rect(surf, color, r.inflate(0, rad*2))
 
 
-def find_best_move(board):
+def find_best_move(board, amount):
     possible_moves = ["L", "R", "U", "D"]
-    totalSims = 48
+    totalSims = amount
 
     simScore = [0,0,0,0]
     inter = 0
@@ -64,117 +69,130 @@ def find_best_move(board):
 
     return bestMove
 
-def simulate():
-    possible_moves = ["L", "R", "U", "D"]
-    totalSims = 24
-
-    simScore = [0,0,0,0]
-    inter = 0
-    for i in range(int(totalSims/4)):
-        print("New simulation")
-        if(inter == 4):
-            inter = 0
-        simulation = Board()
-        simulation.move(possible_moves[inter])
-        simulation.add_block(Block(2 * random.randint(1,2), pg.Rect(0,0, 60, 60)))
-
-        while((simulation.game_over() == False)):
-            
-            
-            simulation.move(possible_moves[random.randint(0,3)])
-            simulation.add_block(Block(2 * random.randint(1,2), pg.Rect(0,0, 60, 60)))
-
-            surface.blit(board, (75, 75))
-
-            simulation.paint(board)
-            pg.display.update()
-        
-            pg.display.flip()
-            clock.tick(30)
-        simScore[inter] += simulation.get_score()
-        inter += 1
-    topScore = max(simScore)
-    print("SimScore",simScore)
-    print("possile",possible_moves)
-
-    index = simScore.index(topScore)
-    bestMove = possible_moves[index]
-
-    return bestMove
-
-pg.init()
-surface = pg.display.set_mode((450, 450))
-clock = pg.time.Clock()
-blue = (115, 147, 179)
 
 
 
-board = pg.Surface((300, 300))
-
-#board.fill((146 ,146, 146))
-
-rect = pg.Rect(0,0,300,300)
-round_rect(board,rect, 10,(100,100,100))
-surface.blit(board, (75, 75))
-
-pos = (-5,0)
+def QUIT():
+    root.destroy()
 
 
 
 
 
-act_board = Board() 
-sim = False
-play = False
+global amount_sims
+amount_sims = 0
+def game():
 
-for i in range(4):
-    for j in range(4):
-        k=60
-        while k > 5:
-            pg.draw.rect(board, (146,146,146),pg.Rect((j*75)+8, (i*75)+8, k,k),2,3)
-            k -= 1
+    game_exit = False
+    BLACK = (0 ,0, 0)
 
-
-if sim:
-    x = simulate()
+    pg.init()
+    surface = pg.display.set_mode((450, 450))
+    clock = pg.time.Clock()
+    blue = (115, 147, 179)
 
 
-while not game_exit and not sim:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            game_exit = True
-        if event.type == pg.MOUSEBUTTONDOWN:
-            if(event.button == 1):
-                pos = pg.mouse.get_pos()
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_LEFT:
-                act_board.shift_left()
-                act_board.add_block(Block(2 * random.randint(1,2), pg.Rect(0,0, 60, 60)))
-            if event.key == pg.K_RIGHT:
-                act_board.shift_right()
-                act_board.add_block(Block(2 * random.randint(1,2), pg.Rect(0,0, 60, 60)))
-            if event.key == pg.K_DOWN:
-                act_board.shift_vertical(False)
-                act_board.add_block(Block(2 * random.randint(1,2), pg.Rect(0,0, 60, 60)))
-            if event.key == pg.K_UP:
-                act_board.shift_vertical(True)
-                act_board.add_block(Block(2 * random.randint(1,2), pg.Rect(0,0, 60, 60)))
-            if event.key == pg.K_c:
-                act_board.clear()
 
-    #this is the code that makes the computer play, change play to True to play the game, play if defined above the while
-    if(not play):
-        l = find_best_move(act_board.board)
-        act_board.move(l)
-        act_board.add_block(Block(2 * random.randint(1,2), pg.Rect(0,0, 60, 60)))
+    board = pg.Surface((300, 300))
 
+    #board.fill((146 ,146, 146))
+
+    rect = pg.Rect(0,0,300,300)
+    round_rect(board,rect, 10,(100,100,100))
     surface.blit(board, (75, 75))
 
+    pos = (-5,0)
 
-    act_board.paint(board)
 
 
-    pg.display.update()
-    
-    pg.display.flip()
-    clock.tick(30)
+
+
+
+
+
+
+
+
+    act_board = Board() 
+    play = False
+
+    for i in range(4):
+        for j in range(4):
+            k=60
+            while k > 5:
+                pg.draw.rect(board, (146,146,146),pg.Rect((j*75)+8, (i*75)+8, k,k),2,3)
+                k -= 1
+
+
+
+    while not game_exit:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                game_exit = True
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if(event.button == 1):
+                    pos = pg.mouse.get_pos()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_LEFT:
+                    act_board.shift_left()
+                    act_board.add_block(Block(2 * random.randint(1,2), pg.Rect(0,0, 60, 60)))
+                if event.key == pg.K_RIGHT:
+                    act_board.shift_right()
+                    act_board.add_block(Block(2 * random.randint(1,2), pg.Rect(0,0, 60, 60)))
+                if event.key == pg.K_DOWN:
+                    act_board.shift_vertical(False)
+                    act_board.add_block(Block(2 * random.randint(1,2), pg.Rect(0,0, 60, 60)))
+                if event.key == pg.K_UP:
+                    act_board.shift_vertical(True)
+                    act_board.add_block(Block(2 * random.randint(1,2), pg.Rect(0,0, 60, 60)))
+                if event.key == pg.K_c:
+                    act_board.clear()
+
+        #this is the code that makes the computer play, change play to True to play the game, play if defined above the while
+        if(not play):
+            l = find_best_move(act_board.board, amount_sims)
+            act_board.move(l)
+            act_board.add_block(Block(2 * random.randint(1,2), pg.Rect(0,0, 60, 60)))
+
+        surface.blit(board, (75, 75))
+
+
+        act_board.paint(board)
+
+
+        pg.display.update()
+        
+        pg.display.flip()
+        clock.tick(30)
+
+
+
+root = tkinter.Tk()
+embed = tk.Frame(root, width = 500, height = 300)
+embed.grid(columnspan = (600), rowspan = 500) 
+embed.pack(side = tk.LEFT) 
+buttonwin = tk.Frame(root, width = 75, height = 500)
+buttonwin.pack(side = tk.LEFT)
+
+lbl=Label(root, text="2048 Monte Carlo Simulations", fg='black', font=("Ariel", 16))
+lbl.place(x=60, y=50)
+root.title('2048 Monte Carlo Simulations')
+
+def change_amount(x):
+    x = txtfld.get()
+    global amount_sims
+    amount_sims = x
+
+txtfld = Entry(root)
+txtfld.bind('<Return>', change_amount)
+txtfld.place(x=80, y=150)
+
+
+button1 = Button(buttonwin,text = 'Quit',  command=QUIT)
+button1.pack(side=LEFT, padx=15, pady=20)
+
+button2 = Button(buttonwin,text = '2048~AI',  command=game)
+button2.pack(side=RIGHT, padx=15, pady=20)
+
+
+root.mainloop()
